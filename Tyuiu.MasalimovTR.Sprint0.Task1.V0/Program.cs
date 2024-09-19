2bb1eeb2229cbb89d037ae917cc49e48b55b71e7
MasalimovTimur <masalimovtr@gmail.com>
2024-09-19 20:49:59 +0500

    Task1

---
+++ Tyuiu.MasalimovTR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.MasalimovTR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            String name = "Тимур";
+            int age = 20;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
