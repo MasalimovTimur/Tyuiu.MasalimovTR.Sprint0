ecb73f75257a711a0b6cc4d2bd879e7a9210d4ae
MasalimovTimur <masalimovtr@gmail.com>
2024-09-20 11:41:25 +0500

    Task4

---
+++ Tyuiu.MasalimovTR.Sprint0.Task4.V0.Lib/DataServise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MasalimovTR.Sprint0.Task4.V0.Lib
+{
+    public class DataServise
+    {
+        public static int Addition(int a, int b)
+            { return a + b; }
+        public static int Subtraction(int a, int b)
+            { return a - b; }
+        public static int Multiolication(int a, int b)
+            { return a * b; }
+        public static Double Division(int a, int b)
+            { return a / b; }
+    }
+}
---
+++ Tyuiu.MasalimovTR.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MasalimovTR.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.MasalimovTR.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataServise.Addition(1, 5));
+            Console.WriteLine(DataServise.Subtraction(15, 5));
+            Console.WriteLine(DataServise.Multiolication(10, 10));
+            Console.WriteLine(DataServise.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
